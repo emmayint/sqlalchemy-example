6a2da3db37aac4bea55f5f667b127d3ffb7419e7
Ting Yin <tiyin@cc7alembic-tour-de-force.cern.ch>
2019-07-01 17:35:58 +0200

    init alembic

---
+++ alembic/versions/68fc3b2489e5_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 68fc3b2489e5
+Revises: 
+Create Date: 2019-07-01 14:59:04.317677
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '68fc3b2489e5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
